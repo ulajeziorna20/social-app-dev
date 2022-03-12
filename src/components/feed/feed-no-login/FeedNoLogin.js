8a45d057bbb32d3d1ea5ce2b0fd8fcbe30eee0a1
Jeziorna Ula <ulajeziorna.de@gmail.com>
2022-03-12 22:15:40 +0100

    4h walczyam z niewidzialnym bledem, ktory raz powodowal ze aplikacja dzialala a raz nie. Byl to blad zwiazany z props missing validation..... Udalo sie w koncu jednak pobrac dane oraz stworzyc tablice z obiektami w ktorych sa odrebnie pobrane dane kazdego uzytkownika z api, z niewiadomych mi przyczyn konsola czesto wyrzuca blad 429 too many PR

--- src/components/feed/feed-no-login/FeedNoLogin.js
+++ src/components/feed/feed-no-login/FeedNoLogin.js
@@ -1,13 +1,41 @@
+import React, { useState } from 'react'
+
 import axios from 'axios'
 
 const FeedNoLogin = (props) => {
+  const [feedNoLogin, setFeedNoLogin] = useState()
+
   const getDate = () => {
     axios
       .post('https://akademia108.pl/api/social-app/post/latest', {
         mode: 'corse'
       })
       .then((res) => {
-        console.log(res)
+        // console.log(res.data)
+        let feedNoLoginList = []
+
+        for (const [userIndex, userData] of Object.entries(res.data)) {
+          // console.log(userIndex)
+          // console.log(userData)
+          // console.log(userData.content)
+          let userFeedNoLogin = {
+            post: {
+              postId: userData.id,
+              content: userData.content,
+              created_at: userData.created_at
+            },
+            user: {
+              userId: userData.user.id,
+              username: userData.user.username,
+              email: userData.user.email,
+              created_at: userData.user.created_at,
+              avatar: userData.user.avatar_url
+            }
+          }
+          feedNoLoginList.push(userFeedNoLogin)
+        }
+        console.log(feedNoLoginList)
+        // setFeedNoLogin(feedNoLoginList)
       })
   }
 
