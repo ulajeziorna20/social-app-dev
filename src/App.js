444644f12ca073ea7171db49dfd6f0f2dff5d873
Jeziorna Ula <ulajeziorna.de@gmail.com>
2022-03-25 20:02:05 +0100

    Zbudowanie formularza do logowania

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
           </li>
         </ul>
       </nav>
-      <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png' alt='fgerger' />
+      {/* <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png' alt='fgerger' /> */}
       <div className='stars'></div>
       <div className='twinkling'></div>
       <div className='clouds'></div>
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,53 @@
 import './Login.css'
 
 const Login = () => {
-  return <>I&apos;am Login</>
+  return (
+    <div>
+      <h2 className='login-title'>Login Form</h2>
+
+      <form className='form-login'>
+        {/* <div className='img-container'>
+          <img
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDlLYn489vbY06yAHAHQ11W6MGlC9Sprs-jw&usqp=CAU'
+            alt='Avatar'
+            className='avatar'
+          />
+        </div> */}
+
+        <div className='container'>
+          <label htmlFor='uname'>
+            <b>Username</b>
+          </label>
+          <input type='text' placeholder='Enter Username' name='uname' id='uname' required />
+
+          <label htmlFor='email'>
+            <b>Email</b>
+          </label>
+          <input type='email' placeholder='Enter email' name='email' id='email' required />
+
+          <label htmlFor='psw'>
+            <b>Password</b>
+          </label>
+          <input type='password' placeholder='Enter Password' name='psw' id='psw' required />
+
+          <button type='submit'>Login</button>
+          <br />
+          <label>
+            <input type='checkbox' name='remember' /> Remember me
+          </label>
+        </div>
+
+        <div className='container'>
+          <button type='button' className='cancelbtn'>
+            Cancel
+          </button>
+          <span className='psw'>
+            Forgot <a href='#'>password?</a>
+          </span>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Login
