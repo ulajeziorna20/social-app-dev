a951dca285276c676c38cb89c9c8da014d0ede13
Jeziorna Ula <ulajeziorna.de@gmail.com>
2022-04-27 16:05:36 +0200

    Wyslanie zapytania, nie wiem tylko dlaczego nie zrwcac mi wlasciwych danych podczas zalogowania tylko caly czas Password/email is required

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import axios from 'axios'
 import './Login.css'
 import { Link } from 'react-router-dom'
 
@@ -11,11 +12,37 @@ const Login = () => {
     setFormData({ ...formData, [name]: value })
   }
 
+  let axiosConfig = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+
+  const handelFormSubmit = (e) => {
+    let postData = {
+      username: formData.username,
+      password: formData.password
+    }
+    console.log(postData.username)
+    console.log(postData.password)
+    axios
+      .post('https://akademia108.pl/api/social-app/user/login', JSON.stringify(postData), JSON.stringify(axiosConfig))
+      .then((response) => {
+        console.log(response)
+        console.log('wysłano zapytanie')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    e.preventDefault()
+  }
+
   return (
     <div>
       <h2 className='login-title'>Login</h2>
 
-      <form className='form-login'>
+      <form className='form-login' onSubmit={handelFormSubmit}>
         <div className='container'>
           <label htmlFor='uname'>
             <b>Username</b>
