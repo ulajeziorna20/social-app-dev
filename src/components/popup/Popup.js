587cd9804a976bbef1e882a08ba95faf9389ea68
Jeziorna Ula <ulajeziorna.de@gmail.com>
2022-05-16 16:38:06 +0200

    Wstawienie komponentu Login do Popupa. Koniec pracy nad Logik Popupa, reszta prac bedzie juz nad Login

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -2,6 +2,8 @@ import './Popup.css'
 
 import { Link } from 'react-router-dom'
 
+import Login from '../login/Login'
+
 const Popup = (props) => {
   return (
     <>
@@ -11,48 +13,7 @@ const Popup = (props) => {
             close
           </div>
           <div className='popup-box solid'>
-            <h2 className='login-title'>Login</h2>
-
-            <form className='form-login'>
-              <div className='container'>
-                <label htmlFor='uname'>
-                  <b>Username</b>
-                </label>
-                <input type='text' placeholder='Enter Username' name='username' id='username' />
-                <p className='error'></p>
-                <br />
-                <label htmlFor='email'>
-                  <b>Email</b>
-                </label>
-                <input type='email' placeholder='Enter email' name='email' id='email' />
-                <p className='error'></p>
-                <br />
-
-                <button type='submit'>Login</button>
-                <br />
-                <label>
-                  <input type='checkbox' name='remember' /> Remember me
-                </label>
-              </div>
-
-              <div className='container-bottom'>
-                <button type='button' className='cancelbtn'>
-                  Cancel
-                </button>
-                <span className='psw'>
-                  Forgot <a href='#'>password?</a>
-                </span>
-                <nav>
-                  <ul>
-                    <li className='nav-item'>
-                      <Link to='/signup' className='link'>
-                        Sign Up!
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </form>
+            <Login />
             <Link to='/sign-up' className='link'>
               Sign Up!
             </Link>
